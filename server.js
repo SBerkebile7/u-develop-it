c56954f08bd97e23f51bcb728dd4f2b131ce0b5b
Spenny Berks <sberkebile@live.com>
2021-07-23 09:14:20 -0400

    further server additions

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,13 +7,27 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'H@yley=1',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
 app.get('/', (req, res) => {
     res.json({
         message: 'Hello World'
     });
 });
 
-app,use((req, res) => {
+app.use((req, res) => {
     res.status(404).end();
 });
 
